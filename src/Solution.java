6d2159e8888953a1aa3ec0d99cc3d41bc45a5970
ks-devanshu <ks.devanshu@gmail.com>
2026-08-09 09:44:39 +0530

    Q-707

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,79 @@
+class MyLinkedList {
+    private class Node {
+        private int value;
+        private Node next;
 
-//Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
-public class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
-            return list2;
-        if (list2 == null)
-            return list1;
-
-        boolean startsWithList1 = true;
-        if (list2.val < list1.val)
-            startsWithList1 = false;
-
-        var primary = startsWithList1 ? list1 : list2;
-        var secondary = startsWithList1 ? list2 : list1;
-        var head = primary;
-        ListNode previous = null;
-
-        while (primary != null && secondary != null) {
-            if (primary.val < secondary.val) {
-                previous = primary;
-                primary = primary.next;
-            }
-            else {
-                var newNode = new ListNode(secondary.val, primary);
-                if (previous == null) {
-                    previous = newNode;
-                    head = previous;
-                }
-                else {
-                    previous.next = newNode;
-                    previous = previous.next;
-                }
-
-                secondary = secondary.next;
-            }
+        public Node() {
         }
-        if (secondary != null)
-            previous.next = secondary;
-        return head;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private final Node dummy;
+    private int count;
+
+    public MyLinkedList() {
+        dummy = new Node();
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= count) return -1;
+        var current = dummy.next;
+        while (index-- > 0) current = current.next;
+        return current.value;
+    }
+
+    public void addAtHead(int val) {
+        dummy.next = new Node(val, dummy.next);
+        count++;
+    }
+
+    public void addAtTail(int val) {
+        var current = dummy;
+        while (current.next != null) current = current.next;
+        current.next = new Node(val);
+        count++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > count) return;
+        if (index == 0) addAtHead(val);
+        else if (index == count) addAtTail(val);
+        else {
+            var current = dummy.next;
+            while (--index > 0) current = current.next;
+            current.next = new Node(val, current.next);
+        count++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= count) return;
+        var current = dummy;
+        Node previous = null;
+        while (index-- >= 0) {
+            previous = current;
+            current = current.next;
+        }
+        previous.next = current.next;
+        current.next = null;
+        count--;
     }
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
