2a66496100071e2a9c22e5e81803e8ba2ca04f85
ks-devanshu <ks.devanshu@gmail.com>
2026-08-09 09:14:34 +0530

    Q-1470

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,14 @@
 public class Solution {
-    private void swap(int i, int k, int[] nums) {
-        int temp = nums[k];
-        nums[k] = nums[i];
-        nums[i] = temp;
-    }
-    public int removeElement(int[] nums, int val) {
-        if (nums.length == 0)
-            return 0;
-        int k = 0, i = 0, n = nums.length;
-        while (i < n) {
-            if (nums[i] != val)
-                swap(i, k++, nums);
-            i++;
+    public int[] shuffle(int[] nums, int n) {
+        int i = 0, j = n, k = 0, len = nums.length;
+        int[] result = new int[len];
+
+        while (k < len) {
+            if (k % 2 == 0)
+                result[k++] = nums[i++];
+            else
+                result[k++] = nums[j++];
         }
-        return k;
+        return result;
     }
 }
