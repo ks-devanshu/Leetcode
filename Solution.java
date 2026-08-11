b495406fceab700ac9ea54ab6f15a457de6edc84
ks-devanshu <ks.devanshu@gmail.com>
2026-08-11 12:31:29 +0530

    Q-700

--- Solution.java
+++ Solution.java
@@ -1,36 +1,26 @@
+class TreeNode {
+ int val;
+ TreeNode left;
+ TreeNode right;
+ TreeNode() {}
+ TreeNode(int val) { this.val = val; }
+ TreeNode(int val, TreeNode left, TreeNode right) {
+     this.val = val;
+     this.left = left;
+     this.right = right;
+ }
+}
+ 
 class Solution {
-    private int[][] matrix;
     private int target;
-    private int m,n;
-    
-    public boolean searchMatrix(int[][] matrix, int target) {
-        this.matrix = matrix;
-        this.target = target;
-        m = matrix.length;
-        n = matrix[0].length;
-        
-        int row = binarySearchOnMatrix(0, m);
-        
-        if (row < 0) return false;
-        
-        return binarySearch(matrix[row], 0, n);
+    public TreeNode searchBST(TreeNode root, int val) {
+        target = val;
+        return searchInBST(root);
     }
-    
-    private int binarySearchOnMatrix(int start, int end) {
-        if (start >= end) return -1;
-        int mid = start + ((end-start)/2);
+    private TreeNode searchInBST(TreeNode root) {
+        if (root == null || root.val == val) return root;
         
-        if (target >= matrix[mid][0] && target <= matrix[mid][n-1]) return mid;
-        if (target < matrix[mid][0]) return binarySearchOnMatrix(start, mid);
-        return binarySearchOnMatrix(mid+1, end);
-    }
-    
-    private boolean binarySearch(int[] nums, int start, int end) {
-        if (start >= end) return false;
-        int mid = start + ((end-start)/2);
-        
-        if (nums[mid] == target) return true;
-        if (target < nums[mid]) return binarySearch(nums, start, mid);
-        return binarySearch(nums, mid+1, end);
+        if (target < root.val) return searchInBST(root.left);
+        return searchInBST(root.right);
     }
 }
