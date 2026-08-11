9737ceef1a41a118733bdef8c891350654dafa85
ks-devanshu <ks.devanshu@gmail.com>
2026-08-11 10:53:31 +0530

    Q-75

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,24 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int num : nums) {
-            queue.add(num);
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return;
+        int left = -1, right = n;
 
-            if (queue.size() > k)
-                queue.remove();
+        int i = 0;
+        while (i < right) {
+            if (nums[i] == 0) {
+                swap(nums, i++, ++left);
+            } else if (nums[i] == 2) {
+                swap(nums, i, --right);
+            } else {
+                i++;
+            }
         }
-        return queue.remove();
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
