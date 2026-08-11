ca45949e1ee9b90855fa5902f5ae32c59653b14e
ks-devanshu <ks.devanshu@gmail.com>
2026-08-11 12:06:40 +0530

    Q-74

--- Solution.java
+++ Solution.java
@@ -1,20 +1,36 @@
 class Solution {
-    private int[] nums;
+    private int[][] matrix;
     private int target;
-    public int search(int[] nums, int target) {
-        this.nums = nums;
+    private int m,n;
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        this.matrix = matrix;
         this.target = target;
-        return binarySearch(0, nums.length);
+        m = matrix.length;
+        n = matrix[0].length;
+        
+        int row = binarySearchOnMatrix(0, m);
+        
+        if (row < 0) return false;
+        
+        return binarySearch(matrix[row], 0, n);
     }
-
-    private int binarySearch(int start, int end) {
+    
+    private int binarySearchOnMatrix(int start, int end) {
         if (start >= end) return -1;
         int mid = start + ((end-start)/2);
-        if (nums[mid] == target)
-            return mid;
-
-        if (target < nums[mid])
-            return binarySearch(start, mid);
-        return binarySearch(mid+1, end);
+        
+        if (target >= matrix[mid][0] && target <= matrix[mid][n-1]) return mid;
+        if (target < matrix[mid][0]) return binarySearchOnMatrix(start, mid);
+        return binarySearchOnMatrix(mid+1, end);
+    }
+    
+    private boolean binarySearch(int[] nums, int start, int end) {
+        if (start >= end) return false;
+        int mid = start + ((end-start)/2);
+        
+        if (nums[mid] == target) return true;
+        if (target < nums[mid]) return binarySearch(nums, start, mid);
+        return binarySearch(nums, mid+1, end);
     }
 }
