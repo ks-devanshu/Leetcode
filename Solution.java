3f5f369d26aa44e2b4f54162eb5471f2e3502971
ks-devanshu <ks.devanshu@gmail.com>
2026-08-14 15:19:42 +0530

    Q-1

--- Solution.java
+++ Solution.java
@@ -1,15 +1,22 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
         
-        for (var num : nums)
-            if (set.contains(num))
-                return true;
-            else set.add(num);
+        for (int i = 0; i<nums.length; i++) {
+            int newTarget = target-nums[i];
+            if (map.containsKey(newTarget)) {
+                result[0] = i;
+                result[1] = map.get(newTarget);
+                break;
+            }
+            else
+                map.put(nums[i], i);
+        }
         
-        return false;
+        return result;
     }
 }
