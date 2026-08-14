9e0c08b1a6bce16605354fecc385144713366daa
ks-devanshu <ks.devanshu@gmail.com>
2026-08-14 22:59:35 +0530

    Q-146

--- Solution.java
+++ Solution.java
@@ -1,22 +1,73 @@
 import java.util.Map;
 import java.util.HashMap;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
+class LRUCache {
+    
+    private class Node {
+        private int key, value;
+        private Node prev, next;
         
-        for (int i = 0; i<nums.length; i++) {
-            int newTarget = target-nums[i];
-            if (map.containsKey(newTarget)) {
-                result[0] = i;
-                result[1] = map.get(newTarget);
-                break;
-            }
-            else
-                map.put(nums[i], i);
+        public Node() {}
+        
+        public Node(int key, int value) {
+            this.value = value;
+            this.key = key;
         }
+    }
+    
+    private void addToTail(Node node) {
+        node.next = tail;
+        node.prev = tail.prev;
         
-        return result;
+        tail.prev = node;
+        node.prev.next = node;
+    }
+    
+    private void delete(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    private Map<Integer, Node> map;
+    private Node head, tail;
+    private int capacity, count;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        
+        var node = map.get(key);
+        delete(node);
+        addToTail(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            var node = map.get(key);
+            delete(node);
+            node.value = value;
+            addToTail(node);
+        }
+        else {
+            var node = new Node(key, value);
+            map.put(key, node);
+            addToTail(node);
+            count++;
+            if (count > capacity) {
+                int toRem = head.next.key;
+                delete(head.next);
+                map.remove(toRem);
+                count--;
+            }
+        }
     }
 }
