d4fe4a0faeccee77c6a658c7cfedb0d7f326820f
ks-devanshu <ks.devanshu@gmail.com>
2026-08-15 17:04:35 +0530

    Q-695

--- Solution.java
+++ Solution.java
@@ -1,30 +1,34 @@
 class Solution {
-    private int islandCount, m, n;
-    private char[][] grid;
-    
-    public int numIslands(char[][] grid) {
+    private int[][] grid;
+    private int maxArea = 0, m, n;
+    public int maxAreaOfIsland(int[][] grid) {
         this.grid = grid;
         m = grid.length;
         n = grid[0].length;
-        for (int i = 0; i<m; i++)
-            for (int j = 0; j<n; j++)
-                if (grid[i][j] == '1') {
-                    dfs(i, j);
-                    islandCount++;
+        
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfs(i, j));
                 }
-        return islandCount;
+            }
+        }
+        return maxArea;
     }
     
-    private void dfs(int row, int col) {
-        if (row < 0 || row >= m || col < 0 || col >= n) return;
+    private int dfs(int row, int col) {
+        if (row < 0 || row >= m || col < 0 || col >= n) return 0;
         
-        if (grid[row][col] == '0' || grid[row][col] == '9') return;
+        if (grid[row][col] == 0 || grid[row][col] == 9) return 0;
         
-        grid[row][col] = '9';
+        int area = 1;
+        grid[row][col] = 9;
         
-        dfs(row, col+1);
-        dfs(row, col-1);
-        dfs(row+1, col);
-        dfs(row-1, col);
+        area += dfs(row, col+1);
+        area += dfs(row, col-1);
+        area += dfs(row+1, col);
+        area += dfs(row-1, col);
+        
+        return area;
     }
 }
