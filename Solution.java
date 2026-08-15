253a5b1531713dab37609c5c3cc80c36c3ea6b79
ks-devanshu <ks.devanshu@gmail.com>
2026-08-15 16:56:33 +0530

    Q-200

--- Solution.java
+++ Solution.java
@@ -1,20 +1,30 @@
-class MyHashMap {
+class Solution {
+    private int islandCount, m, n;
+    private char[][] grid;
     
-    private int[] map;
-
-    public MyHashMap() {
-        map = new int[(10*10*10*10*10*10)+1];
+    public int numIslands(char[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        for (int i = 0; i<m; i++)
+            for (int j = 0; j<n; j++)
+                if (grid[i][j] == '1') {
+                    dfs(i, j);
+                    islandCount++;
+                }
+        return islandCount;
     }
     
-    public void put(int key, int value) {
-        map[key] = value+1;
-    }
-    
-    public int get(int key) {
-        return map[key]-1;
-    }
-    
-    public void remove(int key) {
-        map[key] = 0;
+    private void dfs(int row, int col) {
+        if (row < 0 || row >= m || col < 0 || col >= n) return;
+        
+        if (grid[row][col] == '0' || grid[row][col] == '9') return;
+        
+        grid[row][col] = '9';
+        
+        dfs(row, col+1);
+        dfs(row, col-1);
+        dfs(row+1, col);
+        dfs(row-1, col);
     }
 }
