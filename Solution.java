48d15b90d97deddc2ca6606654f699dffe26dd65
ks-devanshu <ks.devanshu@gmail.com>
2026-08-17 08:15:41 +0530

    'Q-133'

--- Solution.java
+++ Solution.java
@@ -1,51 +1,70 @@
-import java.util.Queue;
-import java.util.ArrayDeque;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
 
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        // 0 - empty, 1 - fresh, 2 rotten
-        int m = grid.length;
-        int n = grid[0].length;
-        int time = 0, fresh = 0;
+    // DFS
+    private Map<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
 
-        Queue<String> queue = new ArrayDeque<>();
-
-        for (int i = 0; i<m; i++) {
-            for (int j = 0; j<n; j++) {
-                if (grid[i][j] == 1)
-                    fresh++;
-                if (grid[i][j] == 2)
-                    queue.add(i+" "+j);
-            }
-        }
-
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (!queue.isEmpty() && fresh > 0) {
-            int k = queue.size();
-
-            for (int i = 0; i<k; i++) {
-                String[] index = queue.remove().split(" ");
-
-                int row = Integer.parseInt(index[0]), col = Integer.parseInt(index[1]);
-
-                for (var diff : directions) {
-                    int nextRow = row + diff[0];
-                    int nextCol = col + diff[1];
-
-                    if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n) continue;
-
-                    if (grid[nextRow][nextCol] != 1) continue;
-
-                    queue.add(nextRow+" "+nextCol);
-                    grid[nextRow][nextCol] = 2;
-                    fresh--;
-                }
-            }
-
-            time++;
-        }
-
-        return fresh > 0 ? -1 : time;
+        return clone(node);
     }
+
+    private Node clone(Node node) {
+        if (map.containsKey(node)) return map.get(node);
+
+        Node copy = new Node(node.val);
+        map.put(node, copy);
+
+        for (var adj : node.neighbors)
+            copy.neighbors.add(clone(adj));
+
+        return copy;
+    }
+
+    //BFS
+    // public Node cloneGraph(Node node) {
+    //     if (node == null) return null;
+
+    //     Queue<Node> queue = new ArrayDeque<>();
+    //     Map<Node, Node> map = new HashMap<>();
+    //     Set<Integer> set = new HashSet<>();
+    //     queue.add(node);
+
+    //     while(!queue.isEmpty()) {
+    //         Node ref = queue.remove();
+    //         Node copy = map.containsKey(ref) ? map.get(ref) : new Node(ref.val);
+    //         map.putIfAbsent(ref, copy);
+
+    //         for (var adj : ref.neighbors) {
+    //             map.putIfAbsent(adj, new Node(adj.val));
+    //             copy.neighbors.add(map.get(adj));
+    //             if (!set.contains(adj.val)) {
+    //                 queue.add(adj);
+    //                 set.add(adj.val);
+    //             }
+    //         }
+    //         set.add(copy.val);
+    //     }
+    //     return map.get(node);
+    // }
 }
