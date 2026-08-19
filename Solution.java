3cc6c849599a65c2d4d015b5f0880b193619dcc3
ks-devanshu <ks.devanshu@gmail.com>
2026-08-19 20:38:38 +0530

    'Q-63'

--- Solution.java
+++ Solution.java
@@ -3,9 +3,11 @@ import java.util.HashMap;
 
 class Solution {
     int m, n;
-    public int uniquePaths(int m, int n) {
-        this.m = m;
-        this.n = n;
+    int[][] grid;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+        grid = obstacleGrid;
 
         return helper(0, 0);
     }
@@ -14,12 +16,12 @@ class Solution {
 
     private int helper(int row, int col) {
         if (row < 0 || row >= m || col < 0 || col >= n) return 0;
-
+        if (grid[row][col] == 1) return 0;
         if (row == m-1 && col == n-1) return 1;
 
         if (map.containsKey(row+" "+col)) return map.get(row+" "+col);
 
-        map.put( row+" "+col , (helper(row, col+1) + helper(row+1, col)));
+        map.put(row+" "+col, (helper(row, col+1) + helper(row+1, col)));
 
         return map.get(row+" "+col);
     }
