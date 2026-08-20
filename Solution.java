ccf1fce51543e41113c5816d2b2a1c98685cff82
ks-devanshu <ks.devanshu@gmail.com>
2026-08-20 15:14:15 +0530

    'Q-191'

--- Solution.java
+++ Solution.java
@@ -1,30 +1,11 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length(), n = text2.length();
-
-        int[] row = new int[m];
-        int i = 0;
-
-        while (i < n) {
-            int left = 0;
-            int[] temp = new int[m];
-            for (int j = 0; j<m; j++){
-                temp[j] = Math.max(row[j], left);
-
-                if (text1.charAt(j) == text2.charAt(i)) {
-                    if (j - 1 < 0 ) temp[j] = 1;
-                    else {
-                        temp[j] = row[j-1] + 1;
-                    }
-                }
-
-                left = temp[j];
-            }
-            row = temp;
-            i++;
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) count++;
+            n = n >> 1;
         }
 
-        if ((m == 1 || n == 1) && row[m-1] > 0) return 1;
-        return row[m-1];
+        return count;
     }
 }
