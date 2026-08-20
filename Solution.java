4138aa6e7a9059d1141a99b5a1d8eafa62d40d3b
ks-devanshu <ks.devanshu@gmail.com>
2026-08-20 15:44:10 +0530

    'Q-190'

--- Solution.java
+++ Solution.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int[] countBits(int n) {
-        int[] result = new int[n+1];
-
-        for (int i = 0; i<=n; i++) {
-            int count = 0;
-            int temp = i;
-            while (temp > 0) {
-                if ((temp & 1) == 1) count++;
-                temp = temp >> 1;
-            }
-            result[i] = count;;
+    public int reverseBits(int n) {
+        int out = 0;
+        for (int i = 0; i < 32; i++) {
+            out = (out << 1) | (n & 1);
+            n >>= 1;
         }
 
-        return result;
+        return out;
     }
 }
