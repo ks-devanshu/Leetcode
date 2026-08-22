ef69d0e7b99719f2197f676f1d999841120e9d27
ks-devanshu <ks.devanshu@gmail.com>
2026-08-22 13:43:21 +0530

    'Q-3622'

--- Solution.java
+++ Solution.java
@@ -1,22 +1,13 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-        int n = nums.length, maxSum = nums[0], minSum = nums[0], currMax = 0, currMin = 0;
-        int i = 0, totalSum = 0;
-
-        while (i < n) {
-            totalSum += nums[i];
-            currMax = Math.max(currMax, 0);
-            currMax += nums[i];
-            maxSum = Math.max(maxSum, currMax);
-
-            currMin = Math.min(currMin, 0);
-            currMin += nums[i];
-            minSum = Math.min(minSum, currMin);
-            i++;
+    public boolean checkDivisibility(int n) {
+        int sum = 0, product = 1;
+        int temp = n;
+        while (temp > 0) {
+            int digit = temp%10;
+            sum += digit;
+            product *= digit;
+            temp = temp / 10;
         }
-
-        if (maxSum < 0) return maxSum;
-
-        return Math.max( maxSum, totalSum - minSum );
+        return (n % (sum + product)) == 0;
     }
 }
