a6f873dbed23e37354ef60a907a7d0bb09fd7514
ks-devanshu <ks.devanshu@gmail.com>
2026-08-23 18:39:06 +0530

    Q-978

--- Solution.java
+++ Solution.java
@@ -1,13 +1,33 @@
 class Solution {
-    public boolean checkDivisibility(int n) {
-        int sum = 0, product = 1;
-        int temp = n;
-        while (temp > 0) {
-            int digit = temp%10;
-            sum += digit;
-            product *= digit;
-            temp = temp / 10;
+    public int maxTurbulenceSize(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return n;
+
+        int i = 0, j = 1;
+        // < -1 > 1 = 0
+        int maxLen = 1;
+        while (j < n) {
+            int prev = arr[j] - arr[j-1];
+            if (prev == 0) {
+                i = j;
+                j++;
+                continue;
+            }
+            if (j+1 >= n) {
+                maxLen = Math.max(maxLen, j-i+1);
+                break;
+            }
+            int next = arr[j+1] - arr[j];
+            if (next == 0 || (next < 0 && prev < 0) || (next > 0 && prev > 0)) {
+                maxLen = Math.max(maxLen, j-i+1);
+                i = j;
+                j++;
+                continue;
+            }
+
+            j++;
         }
-        return (n % (sum + product)) == 0;
+
+        return maxLen;
     }
 }
