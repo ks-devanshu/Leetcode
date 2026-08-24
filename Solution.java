7e10dda200c9e5f70d78737e7930ebf5c21f6f9f
ks-devanshu <ks.devanshu@gmail.com>
2026-08-24 18:40:08 +0530

    Q-209

--- Solution.java
+++ Solution.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int n = arr.length;
-        int i = 0, j = 0,count = 0;
-        int sum = 0;
-        while (j <= n) {
-            if (j-i >= k) {
-                if ((sum/k) >= threshold) count++;
-                sum -= arr[i];
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int minLen = Integer.MAX_VALUE;
+
+        int i = 0, j = 0, sum = 0;
+        while (j < n) {
+            sum += nums[j];
+
+            while (sum >= target) {
+                minLen = Math.min(minLen, j-i+1);
+                sum -= nums[i];
                 i++;
             }
-            if (j >= n) break;
-            sum += arr[j];
+
             j++;
         }
-        return count;
+
+        return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
     }
 }
