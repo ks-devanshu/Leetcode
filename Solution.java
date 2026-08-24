051719ee3ee0756151be5698a1604546f3474e16
ks-devanshu <ks.devanshu@gmail.com>
2026-08-24 14:04:40 +0530

    Q-1343

--- Solution.java
+++ Solution.java
@@ -1,21 +1,18 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n = nums.length;
-        int i = 0, j = 0;
-        Set<Integer> set = new HashSet<>();
-        while (j < n) {
-            if ((j-i) > k) {
-                set.remove(nums[i]);
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int n = arr.length;
+        int i = 0, j = 0,count = 0;
+        int sum = 0;
+        while (j <= n) {
+            if (j-i >= k) {
+                if ((sum/k) >= threshold) count++;
+                sum -= arr[i];
                 i++;
             }
-
-            if (set.contains(nums[j]))
-                return true;
-            else {
-                set.add(nums[j]);
-                j++;
-            }
+            if (j >= n) break;
+            sum += arr[j];
+            j++;
         }
-        return false;
+        return count;
     }
 }
