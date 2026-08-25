33f6c2eafb1595f051cb2758c44acd9fae62afd5
ks-devanshu <ks.devanshu@gmail.com>
2026-08-25 20:07:55 +0530

    Q-42

--- Solution.java
+++ Solution.java
@@ -1,13 +1,22 @@
 class Solution {
-    public int maxArea(int[] height) {
+    public int trap(int[] height) {
         int n = height.length;
-        int left = 0, right = n-1;
-        int maxCapacity = 0;
+        if (n <= 2) return 0;
+
+        int left = 0, right = n-1, leftMax = height[left], rightMax = height[right];
+        int trappedWater = 0;
         while (left < right) {
-            maxCapacity = Math.max(maxCapacity, Math.min(height[left], height[right])*(right-left) );
-            if (height[left] <= height[right]) left++;
-            else right--;
+            if (leftMax <= rightMax) {
+                trappedWater += Math.max(0, leftMax - height[left]);
+                if (height[left] > leftMax) leftMax = height[left];
+                else left++;
+            }
+            else {
+                trappedWater += Math.max(0, rightMax - height[right]);
+                if (height[right] > rightMax) rightMax = height[right];
+                else right--;
+            }
         }
-        return maxCapacity;
+        return trappedWater;
     }
 }
