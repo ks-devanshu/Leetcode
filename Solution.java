f02afbae737dc14247e08a948450a231180fc247
ks-devanshu <ks.devanshu@gmail.com>
2026-08-25 22:07:34 +0530

    Q-303

--- Solution.java
+++ Solution.java
@@ -1,22 +1,18 @@
-class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        if (n <= 2) return 0;
-
-        int left = 0, right = n-1, leftMax = height[left], rightMax = height[right];
-        int trappedWater = 0;
-        while (left < right) {
-            if (leftMax <= rightMax) {
-                trappedWater += Math.max(0, leftMax - height[left]);
-                if (height[left] > leftMax) leftMax = height[left];
-                else left++;
-            }
-            else {
-                trappedWater += Math.max(0, rightMax - height[right]);
-                if (height[right] > rightMax) rightMax = height[right];
-                else right--;
-            }
+class NumArray {
+    int[] prefix;
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n];
+        int sum = 0;
+        for (int i = 0; i<n; i++) {
+            sum += nums[i];
+            prefix[i] = sum;
         }
-        return trappedWater;
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) return prefix[right];
+
+        return prefix[right] - prefix[left-1];
     }
 }
