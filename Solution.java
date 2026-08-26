6b7386941378e53e9d0b0bbde0533c6cdddaebfb
ks-devanshu <ks.devanshu@gmail.com>
2026-08-26 11:57:29 +0530

    Q-724

--- Solution.java
+++ Solution.java
@@ -1,21 +1,18 @@
-class NumMatrix {
-    int[][] prefix;
-    public NumMatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        prefix = new int[m][n];
-        for (int i = 0; i<m; i++) {
-            int sum = 0;
-            for (int j = 0; j<n; j++) {
-                sum += matrix[i][j];
-                int upper = (i-1) < 0 ? 0 : prefix[i-1][j];
-                prefix[i][j] = sum+upper;
-            }
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        int[] prefix = new int[n];
+        int sum = 0;
+        for (int i = 0; i<n; i++) {
+            sum += nums[i];
+            prefix[i] = sum;
         }
-    }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int botLeft = prefix[row2][col2];
-        int result = botLeft - ( (row1 - 1 < 0) ? 0 : prefix[row1-1][col2]) - ( (col1-1 < 0) ? 0 : prefix[row2][col1-1]) + ( (row1-1 < 0 || col1-1 < 0) ? 0 : prefix[row1-1][col1-1]);
-        return result;
+        for (int i = 0; i<n; i++) {
+            int prev = (i-1) < 0 ? 0 : prefix[i-1];
+            if (prev == (prefix[n-1] - prefix[i])) return i;
+        }
+        return -1;
     }
 }
