b4dec65998182391d759f9151c062fe17b0901af
ks-devanshu <ks.devanshu@gmail.com>
2026-08-27 10:12:18 +0530

    Q-876

--- Solution.java
+++ Solution.java
@@ -1,22 +1,10 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        int n = nums.length;
-        int i = 0, sum = 0;
-        int count = 0;
-        while (i < n) {
-            sum += nums[i];
-            int look = sum - k;
-            if (map.containsKey(look))
-                count += map.get(look);
-
-            if (map.containsKey(sum))
-                map.replace(sum, map.get(sum)+1);
-            else
-                map.put(sum, 1);
-            i++;
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return count;
+        return slow;
     }
 }
