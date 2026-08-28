cdcffa751719cb1a19eb3cf1ce5cc814789ddf38
ks-devanshu <ks.devanshu@gmail.com>
2026-08-28 18:25:10 +0530

    Q-745

--- Solution.java
+++ Solution.java
@@ -1,48 +1,80 @@
-class Solution {
-    public List<String> findWords(char[][] board, String[] words) {
-        List<String> res = new ArrayList<>();
-        TrieNode root = buildTrie(words);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                dfs(board, i, j, root, res);
+class WordFilter {
+
+    private class Node{
+        char value;
+        int index;
+        Node parent;
+        Map<Character, Node> children;
+
+        public Node(char value, Node parent) {
+            this.value = value;
+            index = -1;
+            this.parent = parent;
+            children = new HashMap<>();
+        }
+    }
+
+    private Node root = new Node(' ', null);
+
+    private void insert(String word, int index) {
+        Node current = root;
+        for (int i = 0; i<word.length(); i++) {
+            char alpha = word.charAt(i);
+            current.children.putIfAbsent(alpha,new Node(alpha, current));
+            current = current.children.get(alpha);
+        }
+        current.index = index;
+    }
+
+    public WordFilter(String[] words) {
+        for (int i = 0; i<words.length; i++)
+            insert(words[i], i);
+    }
+
+    private String pref, suff;
+    private Set<Integer> set = new HashSet<>();
+
+    public int f(String pref, String suff) {
+        this.pref = pref;
+        this.suff = suff;
+
+        matchPrefix(0, root);
+
+        int maxIndex = -1;
+
+        for (var index : set) {
+            maxIndex = Math.max(maxIndex, index);
+        }
+
+        return maxIndex;
+    }
+
+
+    private void matchSuffix(Node node) {
+        for (var child : node.children.values())
+            matchSuffix(child);
+
+        if (node.index > -1) {
+            Node current = node;
+            int i = suff.length() - 1;
+            while (i >= 0 && current.value == suff.charAt(i)) {
+                current = current.parent;
+                i--;
             }
+            if (i <= 0)
+                set.add(node.index);
         }
-        return res;
     }
 
-    public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-        char c = board[i][j];
-        if (c == '#' || p.next[c - 'a'] == null) { return; }
-        p = p.next[c - 'a'];
-        if (p.word != null) {
-            res.add(p.word);
-            p.word = null;
+
+    private void matchPrefix(int i, Node node) {
+        if (i >= pref.length()) {
+            matchSuffix(node);
+            return;
         }
 
-        board[i][j] = '#';
-        if (i > 0) dfs(board, i - 1, j ,p, res);
-        if (j > 0) dfs(board, i, j - 1, p, res);
-        if (i < board.length - 1) dfs(board, i + 1, j, p, res);
-        if (j < board[0].length - 1) dfs(board, i, j + 1, p, res);
-        board[i][j] = c;
-    }
-
-    public TrieNode buildTrie(String[] words) {
-        TrieNode root = new TrieNode();
-        for (String w: words) {
-            TrieNode p = root;
-            for (char c: w.toCharArray()) {
-                int i = c - 'a';
-                if (p.next[i] == null) { p.next[i] = new TrieNode(); }
-                p = p.next[i];
-           }
-           p.word = w;
-        }
-        return root;
-    }
-
-    class TrieNode {
-        TrieNode[] next = new TrieNode[26];
-        String word;
+        char alpha = pref.charAt(i);
+        if (node.children.containsKey(alpha))
+            matchPrefix(i+1, node.children.get(alpha));
     }
 }
